d14809416061c5ab049f6a7b3b9444559fbfba1d
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-09-02 06:20:58 +0530

    Update gcast.py

--- handlers/gcast.py
+++ handlers/gcast.py
@@ -13,26 +13,18 @@ from config import  SUDO_USERS
     & filters.user(SUDO_USERS)
     & ~ filters.edited
 )
-async def broadcast_message(_, message):
-    if len(message.command) < 2:
-        return await message.reply_text(
-            "**Usage**:\n/broadcast [MESSAGE]"
-        )
-    sleep_time = 0.1
-    text = message.text.split(None, 1)[1]
-    sent = 0
-    schats = await get_all_chnnl()
-    chats = [int(chat["chat_id"]) for chat in schats]
-    m = await message.reply_text(
-        f"Broadcast in progress, will take {len(chats) * sleep_time} seconds."
-    )
-    for i in chats:
-        try:
-            await app.send_message(i, text=text)
-            await asyncio.sleep(sleep_time)
-            sent += 1
-        except FloodWait as e:
-            await asyncio.sleep(int(e.x))
-        except Exception:
-            pass
-    await m.edit(f"**Broadcasted Message In {sent} Chats.**")
+async def sbroadcast(message):
+    data = await get_parsed_note_list(message, split_args=-1)
+    dp.register_message_handler(check_message_for_smartbroadcast)
+
+    await db.sbroadcast.drop({})
+
+    chats = mongodb.chat_list.distinct('chat_id')
+
+    data['chats_num'] = len(chats)
+    data['recived_chats'] = 0
+    data['chats'] = chats
+
+    await db.sbroadcast.insert_one(data)
+    await message.reply("Smart broadcast planned for <code>{}</code> chats".format(len(chats)))
+
