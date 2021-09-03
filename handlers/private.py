c9dec876587c97b18b2a333e1a7182fde423153a
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-09-03 06:18:08 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -1,1 +1,51 @@
 
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+      await message.reply_text(
+        f"""**Hey, I'm {bn} 🎀
+🤖Hᴇʏ, ɪ'ᴍ ʙᴇsᴛɪᴇs ᴠᴄ ʙᴏᴛ❤️🔥
+I Cᴀɴ Pʟᴀʏ Mᴜsɪᴄ Iɴ Yᴏᴜʀ Gʀᴏᴜᴩ Vᴏɪᴄᴇ Cʜᴀᴛ. 
+Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴩ Aɴᴅ Pʟᴀʏ Mᴜsɪᴄ Fʀᴇᴇʟʏ!
+/help - to get commands
+
+        """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Oᴡɴᴇʀ✅", url="https://t.me/ALWAYS_SHIVAMOP")
+                  ],[
+                    InlineKeyboardButton(
+                        "Sᴜᴘᴘᴏʀᴛ🔥", url="https://t.me/BONDOFBESTIZZ"
+                    ),
+                    InlineKeyboardButton(
+                        "Uᴘᴅᴀᴛᴇ🛠", url="https://t.me/INCREDIBLE_SPAM_BOT"
+                    )    
+                ],[ 
+                    InlineKeyboardButton(
+                        "➕Aᴅᴅ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕", url="https://t.me/BESTIES_ROBOT?startgroup=true"
+                    )]
+            ]
+        ),
+     disable_web_page_preview=True
+    )
+
+@Client.on_message(filters.command("start") & ~filters.private & ~filters.channel)
+async def gstart(_, message: Message):
+      await message.reply_text("""**Yes iᴍ online ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔊Uᴩᴅᴀᴛᴇs", url="https://t.me/BONDOFBESTIZZ")
+                ]
+            ]
+        )
+   )
