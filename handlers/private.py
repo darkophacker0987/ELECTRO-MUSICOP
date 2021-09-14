a007b69f335ac1440c50d4e426ee031eb94906bb
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-09-14 22:48:13 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -1,1 +1,20 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
+from config import BOT_NAME as bn
+from helpers.filters import other_filters2
+
+
+@Client.on_message(other_filters2)
+async def start(_, message: Message):
+
+await message.reply_text("""**Yes iᴍ online ✅**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "ßƐSŦĪƐS ZᎾИƐ", url="https://t.me/BONDOFBESTIZZ")
+                ]
+            ]
+        )
+   )
